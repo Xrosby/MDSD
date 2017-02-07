b52147d698a21e884786e633a38fc851e0ba02ed
ulrikpaghschultz <ups@mmmi.sdu.dk>
2017-02-07 11:56:13 +0100

    Fixed borken package decl

--- src/examples/misc/EntitiesLambda.java
+++ src/examples/misc/EntitiesLambda.java
@@ -26,6 +26,7 @@ The views and conclusions contained in the software and documentation are those
 of the authors and should not be interpreted as representing official policies, 
 either expressed or implied, of the University of Southern Denmark.
 */
+package examples.misc;
 
 import java.util.function.Consumer;
 
