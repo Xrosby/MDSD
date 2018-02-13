6e428362b1978417ae44443e5427df7e357e50fb
ulrikpaghschultz <ups@mmmi.sdu.dk>
2018-02-13 10:44:38 +0100

    Fixed indentation

--- src/statemachine/year3/dsl/FluentMachine.java
+++ src/statemachine/year3/dsl/FluentMachine.java
@@ -25,7 +25,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 The views and conclusions contained in the software and documentation are those
 of the authors and should not be interpreted as representing official policies, 
 either expressed or implied, of the University of Southern Denmark.
-*/
+ */
 
 package statemachine.year3.dsl;
 
@@ -46,238 +46,238 @@ public abstract class FluentMachine extends MachineDescription<GenericRuntime> {
 	//
 	// Statemachine metamodel
 	//
-	
+
 	MachineMetaModel metamodel = new MachineMetaModel();
 
 	//
 	// Builder infrastructure
 	//	
-	
-    // Enums defining the types of effects and conditions that can be used
-	
+
+	// Enums defining the types of effects and conditions that can be used
+
 	/**
 	 * Effect of a state: set an extended state variable, or change to a different state
 	 */
-    public enum Effect { SET, CHANGE }
-    /**
-     * Condition on a state: variable equal to the given value or greater than the given value
-     */
-    public enum Condition { EQUAL, GREATER }
+	public enum Effect { SET, CHANGE }
+	/**
+	 * Condition on a state: variable equal to the given value or greater than the given value
+	 */
+	public enum Condition { EQUAL, GREATER }
 
-    // Accumulating variables for the builder
-  
-    /**
-     *  The current state being built
-     */
-    private State<GenericRuntime> currentState;
-    /**
-     *  The current event that transitions are being defined for
-     */
-    private String pendingEvent;
-    /**
-     *  The target of the current transition
-     */
-    private String targetTransition;
-    /**
-     *  The effect of the current transition, if any
-     */
-    private Effect effectMaybe;
-    /**
-     *  The constant argument to the effect on the current transition, if any
-     */
-    private int effectArgument;
-    /**
-     *  The name of the mutable state acted upon by the effect on the current transition, if any
-     */
-    private String effectVariable;
-    /**
-     * A factory object for creating transition instances, overwrite default to control transition creation
-     */
+	// Accumulating variables for the builder
+
+	/**
+	 *  The current state being built
+	 */
+	private State<GenericRuntime> currentState;
+	/**
+	 *  The current event that transitions are being defined for
+	 */
+	private String pendingEvent;
+	/**
+	 *  The target of the current transition
+	 */
+	private String targetTransition;
+	/**
+	 *  The effect of the current transition, if any
+	 */
+	private Effect effectMaybe;
+	/**
+	 *  The constant argument to the effect on the current transition, if any
+	 */
+	private int effectArgument;
+	/**
+	 *  The name of the mutable state acted upon by the effect on the current transition, if any
+	 */
+	private String effectVariable;
+	/**
+	 * A factory object for creating transition instances, overwrite default to control transition creation
+	 */
 	private TransitionFactory factory = new TransitionFactory();
 	/**
 	 * Flag indicating whether the model has been built
 	 */
 	private boolean modelIsBuilt = false;
-	
-    /**
-     * Build a machine using the fluent interface.  First call build (overridden in subclass),
-     * then ensure that all transitions associated with the current state have been defined,
-     * and last add the state as the last in the list of states
-     */
-    public FluentMachine() {
-    }
-    
-    protected void buildMachine() {
-    	if(modelIsBuilt) return;
-        build();
-        flushTransition(null,null,0);
-        if(currentState==null) throw new Error("Empty statemachine definition");
-        metamodel.getAllStates().add(currentState);
-        modelIsBuilt = true;
-    }
-    
-    /**
-     * Get list of all states in the state machine
-     */
-    @Override
-	public List<State<GenericRuntime>> getAllStates() {
-    	buildMachine();
-        return metamodel.getAllStates();
-    }
 
-    /**
-     * Create an instance of the extended state required for this statemachien
-     */
+	/**
+	 * Build a machine using the fluent interface.  First call build (overridden in subclass),
+	 * then ensure that all transitions associated with the current state have been defined,
+	 * and last add the state as the last in the list of states
+	 */
+	public FluentMachine() {
+	}
+
+	protected void buildMachine() {
+		if(modelIsBuilt) return;
+		build();
+		flushTransition(null,null,0);
+		if(currentState==null) throw new Error("Empty statemachine definition");
+		metamodel.getAllStates().add(currentState);
+		modelIsBuilt = true;
+	}
+
+	/**
+	 * Get list of all states in the state machine
+	 */
+	@Override
+	public List<State<GenericRuntime>> getAllStates() {
+		buildMachine();
+		return metamodel.getAllStates();
+	}
+
+	/**
+	 * Create an instance of the extended state required for this statemachien
+	 */
 	@Override
 	protected GenericRuntime createExtendedState() {
 		return new GenericRuntime(this.metamodel.getExtendedStateVariables());
 	}
-    
-    /**
-     * Override in subclasses, must define state machine using fluent interface (and initialize state variables)
-     */
-    protected abstract void build();
-    
-    /**
-     * Start a new state, of the given name
-     */
-    public FluentMachine state(String name) {
-        if(currentState!=null) {
-            flushTransition(null,null,0);
-            metamodel.getAllStates().add(currentState);
-        }
-        currentState = new State<GenericRuntime>(name);
-        return this;
-    }
-    
-    /**
-     * Define a new transition, in the context of the current state
-     */
-    public FluentMachine transition(String event) {
-        if(targetTransition!=null || effectMaybe!=null) flushTransition(null,null,0);
-        pendingEvent = event;
-        return this;
-    }
-    
-    /**
-     * Name the target state of the current transition
-     */
-    public FluentMachine to(String state) {
-        targetTransition = state;
-        return this;
-        
-    }
 
-    /**
-     * Include a set state effect in the current transition
-     * @param variable the variable to set
-     * @param value the value to set it to
-     */
-    public FluentMachine setState(String variableName, int value) {
-        effectMaybe = Effect.SET;
-        if(!this.metamodel.getExtendedStateVariables().contains(variableName)) throw new Error("Undefined variable: "+variableName);
-        effectVariable = variableName;
-        effectArgument = value;
-        return this;
-    }
+	/**
+	 * Override in subclasses, must define state machine using fluent interface (and initialize state variables)
+	 */
+	protected abstract void build();
 
-    /**
-     * Include a change state effect in the current transition
-     * @param variable the variable to change
-     * @param value the value to add to the variable
-     */
-    public FluentMachine changeState(String variableName, int value) {
-        effectMaybe = Effect.CHANGE;
-        if(!this.metamodel.getExtendedStateVariables().contains(variableName)) throw new Error("Undefined variable: "+variableName);
-        effectVariable = variableName;
-        effectArgument = value;
-        return this;
-    }
-    
-    /**
-     * Add an equals condition to the current transition
-     * @param variable the variable to have a condition on
-     * @param value the value to compare to
-     */
-    public FluentMachine whenStateEquals(String variableName, int value) {
-        flushTransition(Condition.EQUAL,variableName,value);
-        return this;
-    }
-    
-    /**
-     * Add a comparison condition to the current transition
-     * @param variable the variable to have a condition on
-     * @param value the value to compare to
-     */
-    public FluentMachine whenStateGreaterThan(String variableName, int value) {
-        flushTransition(Condition.GREATER,variableName,value);
-        return this;
-    }
-    
-    /**
-     * Indicate that a state is a simple alternative (syntactic sugar)
-     */
-    public FluentMachine otherwise() {
-        flushTransition(null,null,0);
-        return this;
-    }
+	/**
+	 * Start a new state, of the given name
+	 */
+	public FluentMachine state(String name) {
+		if(currentState!=null) {
+			flushTransition(null,null,0);
+			metamodel.getAllStates().add(currentState);
+		}
+		currentState = new State<GenericRuntime>(name);
+		return this;
+	}
 
-    /**
-     * Flush the current transition, in preparation for the start of a new transition or state
-     * @param cond the condition type, if any, on the transition being finalized
-     * @param condVariableMaybe the variable to test on, if any
-     * @param condValue the value to compare to, if any
-     */
-    private void flushTransition(Condition cond, String condVariableNameMaybe, int condValue) {
-        if(pendingEvent==null) return; // Nothing to flush
-        if(targetTransition==null && effectMaybe==null) return; // empty transition
-        // Define transition and add to current state
-        Transition<GenericRuntime> transition = 
-        		factory.createTransitionHook(targetTransition, 
-        				effectMaybe, effectVariable, effectArgument, 
-        				cond, condVariableNameMaybe, condValue);
-        currentState.addTransition(pendingEvent, transition);
-        // Clear all context variables
-        effectMaybe = null;
-        effectVariable = null;
-        targetTransition = null;
-    }
+	/**
+	 * Define a new transition, in the context of the current state
+	 */
+	public FluentMachine transition(String event) {
+		if(targetTransition!=null || effectMaybe!=null) flushTransition(null,null,0);
+		pendingEvent = event;
+		return this;
+	}
 
-    /**
-     * Change the factory used by the builder to create transition objects
-     * @param factory the new transition factory
-     */
-    public void setTransitionFactory(TransitionFactory factory) {
-    	this.factory = factory;
-    }
-    
-    /**
-     * Add a new integer state to the state machine
-     * @param name the name of the extended state variable
-     */
-    public void integerState(String name) {
-    	this.metamodel.getExtendedStateVariables().add(name);
-    }
-    
-    public static class TransitionFactory {
-    	/**
-    	 * Hook method allowing the creation of a transition to be changed, as specified by the arguments.
-    	 * @param target the target of the transition
-    	 * @param effect the effect of the transition, if any
-    	 * @param effectVar the variable that the transition has an effect on, if any
-    	 * @param effectArg the argument of the effect, if any
-    	 * @param cond the condition of the transition, if any
-    	 * @param condVariableMaybe the variable used in the condition, if any
-    	 * @param condValue the value used in the condition, if any
-    	 * @return a transition object created according to the specification.
-    	 */
-    	protected Transition<GenericRuntime> createTransitionHook(String target, 
-    			Effect effect, String effectVarName, int effectArg, 
-    			Condition cond, String condVariableMaybe, int condValue) {
-    		return new GenericTransition(target,
-    				effect,effectVarName,effectArg,
-    				cond, condVariableMaybe,condValue);
-    	}
-    }
+	/**
+	 * Name the target state of the current transition
+	 */
+	public FluentMachine to(String state) {
+		targetTransition = state;
+		return this;
+
+	}
+
+	/**
+	 * Include a set state effect in the current transition
+	 * @param variable the variable to set
+	 * @param value the value to set it to
+	 */
+	public FluentMachine setState(String variableName, int value) {
+		effectMaybe = Effect.SET;
+		if(!this.metamodel.getExtendedStateVariables().contains(variableName)) throw new Error("Undefined variable: "+variableName);
+		effectVariable = variableName;
+		effectArgument = value;
+		return this;
+	}
+
+	/**
+	 * Include a change state effect in the current transition
+	 * @param variable the variable to change
+	 * @param value the value to add to the variable
+	 */
+	public FluentMachine changeState(String variableName, int value) {
+		effectMaybe = Effect.CHANGE;
+		if(!this.metamodel.getExtendedStateVariables().contains(variableName)) throw new Error("Undefined variable: "+variableName);
+		effectVariable = variableName;
+		effectArgument = value;
+		return this;
+	}
+
+	/**
+	 * Add an equals condition to the current transition
+	 * @param variable the variable to have a condition on
+	 * @param value the value to compare to
+	 */
+	public FluentMachine whenStateEquals(String variableName, int value) {
+		flushTransition(Condition.EQUAL,variableName,value);
+		return this;
+	}
+
+	/**
+	 * Add a comparison condition to the current transition
+	 * @param variable the variable to have a condition on
+	 * @param value the value to compare to
+	 */
+	public FluentMachine whenStateGreaterThan(String variableName, int value) {
+		flushTransition(Condition.GREATER,variableName,value);
+		return this;
+	}
+
+	/**
+	 * Indicate that a state is a simple alternative (syntactic sugar)
+	 */
+	public FluentMachine otherwise() {
+		flushTransition(null,null,0);
+		return this;
+	}
+
+	/**
+	 * Flush the current transition, in preparation for the start of a new transition or state
+	 * @param cond the condition type, if any, on the transition being finalized
+	 * @param condVariableMaybe the variable to test on, if any
+	 * @param condValue the value to compare to, if any
+	 */
+	private void flushTransition(Condition cond, String condVariableNameMaybe, int condValue) {
+		if(pendingEvent==null) return; // Nothing to flush
+		if(targetTransition==null && effectMaybe==null) return; // empty transition
+		// Define transition and add to current state
+		Transition<GenericRuntime> transition = 
+				factory.createTransitionHook(targetTransition, 
+						effectMaybe, effectVariable, effectArgument, 
+						cond, condVariableNameMaybe, condValue);
+		currentState.addTransition(pendingEvent, transition);
+		// Clear all context variables
+		effectMaybe = null;
+		effectVariable = null;
+		targetTransition = null;
+	}
+
+	/**
+	 * Change the factory used by the builder to create transition objects
+	 * @param factory the new transition factory
+	 */
+	public void setTransitionFactory(TransitionFactory factory) {
+		this.factory = factory;
+	}
+
+	/**
+	 * Add a new integer state to the state machine
+	 * @param name the name of the extended state variable
+	 */
+	public void integerState(String name) {
+		this.metamodel.getExtendedStateVariables().add(name);
+	}
+
+	public static class TransitionFactory {
+		/**
+		 * Hook method allowing the creation of a transition to be changed, as specified by the arguments.
+		 * @param target the target of the transition
+		 * @param effect the effect of the transition, if any
+		 * @param effectVar the variable that the transition has an effect on, if any
+		 * @param effectArg the argument of the effect, if any
+		 * @param cond the condition of the transition, if any
+		 * @param condVariableMaybe the variable used in the condition, if any
+		 * @param condValue the value used in the condition, if any
+		 * @return a transition object created according to the specification.
+		 */
+		protected Transition<GenericRuntime> createTransitionHook(String target, 
+				Effect effect, String effectVarName, int effectArg, 
+				Condition cond, String condVariableMaybe, int condValue) {
+			return new GenericTransition(target,
+					effect,effectVarName,effectArg,
+					cond, condVariableMaybe,condValue);
+		}
+	}
 
 }
